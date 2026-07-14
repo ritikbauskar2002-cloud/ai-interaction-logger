075a6dc605dbc65a45070b19ee1a649e5c461e15
Ritik Bauskar <ritikbauskar@gmail.com>
2026-07-14 14:48:03 +0530

    Create project folder structure

--- backend/app/api/chat.py
+++ backend/app/api/chat.py
@@ -1,12 +1,23 @@
 from fastapi import APIRouter
+
+from app.graph.builder import build_graph
+from app.graph.state import GraphState
 from app.models.interaction import Interaction
 
 router = APIRouter()
 
+graph = build_graph()
+
 
 @router.post("/chat")
 def chat(interaction: Interaction):
-    return {
-        "status": "success",
-        "data": interaction
+
+    state: GraphState = {
+        "message": interaction.notes,
+        "interaction": interaction,
+        "response": ""
     }
+
+    result = graph.invoke(state)
+
+    return result
